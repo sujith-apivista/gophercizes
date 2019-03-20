915743072294165345fb80c43a79b58cb9ebd24f
Sujith Paul <sujith.paul@apivista.com>
2019-03-20 16:44:45 -0400

    Finish part 2 of problem 1

--- ex-1-quiz/quiz.go
+++ ex-1-quiz/quiz.go
@@ -8,20 +8,28 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
-	var filename = flag.String("fn", "problems.csv", "the input file")
+	var filename = flag.String("fn", "problems.csv", "the input CSV file with questions and answers")
+	var timelimit = flag.Int("t", 30, "time limit in seconds")
 	flag.Parse()
 
 	f, err := os.Open(*filename)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Print("Press ENTER to start.....")
+	fmt.Scanln()
+	timer := time.NewTimer(time.Duration(*timelimit) * time.Second)
+
 	r := csv.NewReader(f)
 	scanner := bufio.NewScanner(os.Stdin)
 	correct, total := 0, 0
 
+outerloop:
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -33,16 +41,26 @@ func main() {
 
 		fmt.Printf("%v ", record[0])
 		total++
-		scanner.Scan()
-		answer := scanner.Text()
-		if answer == record[1] {
-			fmt.Println("Correct!")
-			correct++
-		} else {
-			fmt.Println("Wrong!")
+		ansChannel := make(chan string)
+
+		go func() {
+			scanner.Scan()
+			ansChannel <- scanner.Text()
+		}()
+
+		select {
+		case <-timer.C:
+			break outerloop
+		case answer := <-ansChannel:
+			if answer == record[1] {
+				fmt.Println("Correct!")
+				correct++
+			} else {
+				fmt.Println("Wrong!")
+			}
 		}
 	}
-	fmt.Printf("You scored %v out of %v\n", correct, total)
+	fmt.Printf("\nYou scored %v out of %v\n", correct, total)
 
 	if err := f.Close(); err != nil {
 		log.Fatal(err)
